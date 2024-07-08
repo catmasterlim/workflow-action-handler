326d4894ec02a61d21d765cd29bb79b69302644a
catmasterlim <catmasterlim@gmail.com>
2024-07-08 16:40:35 +0900

    - complate : action args 클래스 별로 처리   -> condition, validator, postfunction

--- src/main/java/dev/impl/SoyFunctions.java
+++ src/main/java/dev/impl/SoyFunctions.java
@@ -1,8 +1,10 @@
 package dev.impl;
 
+import com.atlassian.annotations.PublicSpi;
+
 import java.util.List;
 
-
+@PublicSpi
 public class SoyFunctions {
     public static boolean includes(List list, Object item) {
         if(list == null || list.isEmpty()){
